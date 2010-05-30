dcbe1f1e2fc5bdee081ec5a505bd5418d4278213
unknown <Administrator@.(none)>
2010-05-30 16:16:27 +0200

    Updated the state-hiding mechanism and public interface export method using closures in Clojure.

--- src/test/clojure/whiteboard/y2010/hide-adt-state-using-closure.clj
+++ src/test/clojure/whiteboard/y2010/hide-adt-state-using-closure.clj
@@ -1,13 +1,17 @@
 (ns com.khakbaz.algorithms.test.whiteboard.y2010)
 
-(def public-a nil)
+(defn make-module []
+  (let [state 0]
+    (defn public-operation [x y]
+      (println "I'm a public operation on this datatype.")
+      (println "I consume the following arguments:" x y))
+    (defn say-hello []
+      (println "Hey Buddy!"))
+    (fn [msg]
+      (case msg
+        :hello say-hello
+        :pub public-operation))))
 
-(binding
-  [state 0]
-
-  (set! public-a
-    (fn [x]
-      (println "I'm a public member of this adt.")))
-
-  (println public-a)
-  )
+(def dt (make-module))
+(def hello (dt :hello))
+(def pub (dt :pub))
