0dad5c63ea0308c0abdf8789b28ddda02af161ce
unknown <Administrator@.(none)>
2010-05-31 10:15:38 +0200

    Adopted Meikel's suggestion: http://groups.google.com/group/clojure/msg/c274fcaffd183e14?hl=en

--- src/test/clojure/whiteboard/y2010/hide-adt-state/datatype-in-closure-01.clj
+++ src/test/clojure/whiteboard/y2010/hide-adt-state/datatype-in-closure-01.clj
@@ -1,10 +1,11 @@
+(def #^{:private true} state (atom 10))
+
 (defprotocol prot-a
   (op-a [self x y]))
 
-(let [state (atom 10)]
-  (deftype t-a [member]
-    prot-a
-    (op-a [self x y]
-      (+ (.member self) x y @state))))
+(deftype t-a [member]
+  prot-a
+  (op-a [self x y]
+    (+ (.member self) x y @state)))
 
 (def t-a-instance (t-a. 5))
