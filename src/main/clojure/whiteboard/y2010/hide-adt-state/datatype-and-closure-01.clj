b217b1d5f22726b7d87b10be8eebbc95428ad1c2
Sina K. Heshmati <sina@khakbaz.com>
2010-05-31 17:10:36 +0200

    Got extend out of the closure and got rid of declare as suggested by Meikel

--- src/main/clojure/whiteboard/y2010/hide-adt-state/datatype-and-closure-01.clj
+++ src/main/clojure/whiteboard/y2010/hide-adt-state/datatype-and-closure-01.clj
@@ -1,7 +1,5 @@
 (ns com.khakbaz.algorithms.clojure.whiteboard.y2010.hide-adt-state.datatype-and-closure-01)
 
-(declare op-a)
-
 (defprotocol prot-a
   (op-a [self x y]))
 
@@ -10,10 +8,10 @@
 (let [state (atom 10)]
   (defn op-a
     [this x y]
-    (+ (.member this) x y @state))
+    (+ (.member this) x y @state)))
 
-  (extend t-a
-    prot-a
-    {:op-a op-a}))
+(extend t-a
+  prot-a
+  {:op-a op-a})
 
 (def t-a-1 (t-a. 5))
