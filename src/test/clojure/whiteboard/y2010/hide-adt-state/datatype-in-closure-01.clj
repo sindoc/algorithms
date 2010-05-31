feac3c4e64ebcfee05be7c3f7a0cb18af8ce0cf7
unknown <Administrator@.(none)>
2010-05-31 09:32:25 +0200

    Further simplified the example. The atomic 'state' is NOT visible to datatype operations, why?

--- src/test/clojure/whiteboard/y2010/hide-adt-state/datatype-in-closure-01.clj
+++ src/test/clojure/whiteboard/y2010/hide-adt-state/datatype-in-closure-01.clj
@@ -1,17 +1,10 @@
 (defprotocol prot-a
   (op-a [self x y]))
 
-(defprotocol prot-b
-  (op-b [self x]))
-
 (let [state (atom 10)]
-
   (deftype t-a [member]
     prot-a
     (op-a [self x y]
-      (+ (.member self) x y))
-    prot-b
-    (op-b [self x]
-      (+ (.member self) x @state))))
+      (+ (.member self) x y @state))))
 
 (def t-a-instance (t-a. 5))
